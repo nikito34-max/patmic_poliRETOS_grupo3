cc5b82f4cb2e097efb90ab39d8e879451b1d4d2a
Niko <nikoortegon59@gmail.com>
2025-11-02 21:46:41 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# patmic_poliRETOS - Grupo 3
+
+## Información sobre los archivos
+
+En la carpeta 'prjPoliRetos_grupo3' se encontrará todo lo realizado con respecto al proyecto de los poliRETOS, habrán 8 clases, cada una corresponde a cada sección del trabajo. En la carpeta 'evidencias' estarán los archivos .pdf de los códigos realizados a mano y 'App.java' es el archivo final ejecutable.
+
+## División del trabajo
+
+La división del trabajo es la siguiente:
+
+- Fernando Paguay: Series numéricas y series de caracteres.
+- Stefanie Ortega: Figuras.
+- Nikolai Ortega: Cadenas de caracteres y arrays.
+- Angelo Muñoz: Loading.
+- Danny Pineda: Recursión, grafos y autómatas.
+
---
+++ prjPoliRetos_grupo3/src/App.java
@@ -0,0 +1,140 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        Figuras fig = new Figuras();
+        CadenaCaracteres cadCar = new CadenaCaracteres();
+        SeccionArrays secArr = new SeccionArrays();
+        LoadingDelay loadDel = new LoadingDelay();
+        Recursion rec = new Recursion();
+        GrafosYAutomatas grfAuto = new GrafosYAutomatas();
+        SeriesNumericas serNum = new SeriesNumericas();
+        SeriesCaracteres serCar = new SeriesCaracteres();
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("patmic_poliRETOS por parte del Grupo 3\nAntes de iniciar, ingresa los siguientes datos:");
+        System.out.print("Numero de términos: ");
+        int num_terminos = entrada.nextInt();
+        System.out.print("Numero de niveles: ");
+        int niveles = entrada.nextInt();
+        System.out.print("ancho: ");
+        int ancho = entrada.nextInt();
+        System.out.print("largo: ");
+        int largo = entrada.nextInt();
+        System.out.print("Ingresa un caracter: ");
+        char caracter = entrada.next().charAt(0);
+
+        System.out.print("Ingresa una frase: ");
+        String frase = entrada.nextLine();
+        entrada.nextLine();
+        System.out.print("Tu nombre: ");
+        String nombre = entrada.nextLine();
+        System.out.print("Tu apellido: ");
+        String apellido = entrada.nextLine();
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //lo de arriba es para las entradas necesarias, agregar mas o modificar si sus ejercicios lo piden, luego se pasaran como parametros
+        // A continuacion, se llamaran a los metodos de los objetos con los parametros establecidos al inicio de este archivo (entradas),
+        //creados al principio, por ejemplo, fig.g3_mostrarFigura1(ancho, largo), serNum.crearSerie3(numTerminos), serCar.invertirFrase(frase), etc
+        //Se dividira de acuerdo al numero de ejercicios de cada seccion, hay que respetar los espacios para evitar conflictos del merge.
+        //Empezamos desde la linea 52, que corresponde al inicio de la primera seccion, eliminar estos comentarios de seccion-fin luego de haber puesto su parte ya que no son permitidos.
+        //Seccion 1
+
+
+
+
+
+
+
+
+
+
+        //FIN
+        
+        //Seccion 2
+
+
+
+
+
+
+
+        //FIN
+
+        //Seccion 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        //FIN
+
+        //Seccion 4
+
+
+
+
+
+
+
+        //FIN
+
+        //Seccion 5
+
+
+
+        //FIN
+
+        //Seccion 6
+
+
+
+
+
+
+
+
+
+
+
+        //FIN
+
+        //Seccion 7
+
+
+
+
+        //FIN
+
+        //Seccion 8 
+
+
+
+
+
+        //FIN
+    }
+}
