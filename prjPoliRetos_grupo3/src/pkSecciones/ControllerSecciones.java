f20f57dd7b2a1934b45ac24853ba8c91f7a2b29e
Niko <nikoortegon59@gmail.com>
2025-11-12 01:08:43 -0500

    Controller general agregado

---
+++ prjPoliRetos_grupo3/src/pkSecciones/ControllerSecciones.java
@@ -0,0 +1,88 @@
+package pkSecciones;
+
+import java.util.Scanner;
+
+import pkSecciones.Seccion1.ControllerSeccion1;
+import pkSecciones.Seccion2.ControllerSeccion2;
+import pkSecciones.Seccion3.ControllerSeccion3;
+import pkSecciones.Seccion4.ControllerSeccion4;
+import pkSecciones.Seccion5.ControllerSeccion5;
+import pkSecciones.Seccion6.ControllerSeccion6;
+import pkSecciones.Seccion7.ControllerSeccion7;
+import pkSecciones.Seccion8.ControllerSeccion8;
+
+public class ControllerSecciones {
+    public void ejecutar() {
+
+        System.out.println("Iniciando Proyecto PoliRETOS Grupo 3");
+        System.out.println("\nIntegrantes:\nmuñoz.angelo\nortega.stefanie\nortega.nikolai\npaguay.fernando\npineda.danny\nAntes de iniciar, ingresa los siguientes datos generales:");
+     
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Numero de términos: ");
+        int num_terminos = entrada.nextInt();
+        entrada.nextLine(); 
+        System.out.print("Ingresa una frase: ");  
+        String frase = entrada.nextLine();
+
+        boolean salir = false;
+        while (!salir) {
+            System.out.println();
+            System.out.println("Seleccione el módulo:");
+            System.out.println("\t1 - Series Numericas");
+            System.out.println("\t2 - Series de Caracteres");
+            System.out.println("\t3 - Figuras");
+            System.out.println("\t4 - Cadena de Caracteres");
+            System.out.println("\t5 - Arrays");
+            System.out.println("\t6 - Loading");
+            System.out.println("\t7 - Recursion");
+            System.out.println("\t8 - Automatas");
+            System.out.println("\t9 - SALIR DEL PROGRAMA");
+            System.out.print("> ");
+            int opcion = entrada.nextInt();
+    
+            switch (opcion) {
+                case 1:
+                    System.out.println("=== SERIES NUMERICAS ===");
+                    new ControllerSeccion1(num_terminos).iniciarSeccion1();
+                    break;
+                case 2:
+                    System.out.println("=== SERIES DE CARACTERES ===");
+                    new ControllerSeccion2(num_terminos).iniciarSeccion2();
+                    break;
+                case 3:
+                    System.out.println("=== FIGURAS ===");
+                     ControllerSeccion3 controller = new ControllerSeccion3();
+                     controller.inicializarfiguras();
+                     break;
+                case 4:
+                    System.out.println("=== CADENAS DE CARACTERES ===");
+                    new ControllerSeccion4(frase, entrada);
+                    break;
+                case 5:
+                    System.out.println("=== ARRAYS ===");
+                    new ControllerSeccion5(entrada);
+                    break;
+                case 6:
+                    new ControllerSeccion6();
+                    break;
+                case 7:
+                    System.out.println("=== RECURSION ===");
+                    new ControllerSeccion7().g3_ejecutar();
+                    break;
+                case 8:
+                    System.out.println("=== AUTOMATAS ===");
+                    new ControllerSeccion8().g3_ejecutar();
+                    break;
+                case 9:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("Opción inválida.");
+                    break;
+            }
+    }
+    System.out.println("\nProyecto Finalizado");
+        entrada.close();
+ }
+}
+
--- prjPoliRetos_grupo3/src/pkSecciones/Seccion1/ControllerSeccion1.java
+++ prjPoliRetos_grupo3/src/pkSecciones/Seccion1/ControllerSeccion1.java
@@ -1,4 +1,5 @@
 package pkSecciones.Seccion1;
+
 public class ControllerSeccion1 {
     private SeriesNumericas serNum;
 
@@ -7,48 +8,48 @@ public class ControllerSeccion1 {
     }
 
     public void iniciarSeccion1() {
-        System.out.print("[+] S1 ");
-        SeriesNumericas.g3_serieFibonacci();
+        System.out.print("[+] S1: ");
+        serNum.g3_serieFibonacci();
         System.out.println();
 
         System.out.print("[+] S2: ");
-        SeriesNumericas.g3_serieAlternaConCeros();
+        serNum.g3_serieAlternaConCeros();
         System.out.println();
 
         System.out.print("[+] S3: ");
-        SeriesNumericas.g3_serieFraccionesS3();
+        serNum.g3_serieFraccionesS3();
         System.out.println();
 
         System.out.print("[+] S4: ");
-        SeriesNumericas.g3_serieFraccionesS4();
+        serNum.g3_serieFraccionesS4();
         System.out.println();
 
         System.out.print("[+] S5: ");
-        SeriesNumericas.g3_seriePrimos();
+        serNum.g3_seriePrimos();
         System.out.println();
 
         System.out.print("[+] S6: ");
-        SeriesNumericas.g3_serieCuadrados();
+        serNum.g3_serieCuadrados();
         System.out.println();
 
         System.out.print("[+] S7: ");
-        SeriesNumericas.g3_serieAritmeticaS7();
+        serNum.g3_serieAritmeticaS7();
         System.out.println();
 
         System.out.print("[+] S8: ");
-        SeriesNumericas.g3_serieAritmeticaS8();
+        serNum.g3_serieAritmeticaS8();
         System.out.println();
 
         System.out.print("[+] S9: ");
-        SeriesNumericas.g3_seriePotenciasDos();
+        serNum.g3_seriePotenciasDos();
         System.out.println();
 
         System.out.print("[+] S10: ");
-        SeriesNumericas.g3_seriePotenciasTres();
+        serNum.g3_seriePotenciasTres();
         System.out.println();
 
         System.out.print("[+] S11: ");
-        SeriesNumericas.g3_seriePoligonal();
+        serNum.g3_seriePoligonal();
         System.out.println();
     }
 }
